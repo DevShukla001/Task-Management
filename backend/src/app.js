ed5f8cff813dabce7e07a7f6ec49afa84f4064d0
DevShukla001 <devshukla2737@gmail.com>
2026-05-18 21:27:53 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: *,
   credentials: true
 }));
 app.use(helmet());
