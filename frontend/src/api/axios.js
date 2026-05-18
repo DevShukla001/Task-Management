6bdfb2628173f3ffc4509937800f15f5e9450962
DevShukla001 <devshukla2737@gmail.com>
2026-05-18 21:19:56 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
 // const API = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL
+//   baseURL: "http://localhost:5000/api",
 // });
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+});
 API.interceptors.request.use((config) => {
   const userInfo = localStorage.getItem("userInfo");
 
